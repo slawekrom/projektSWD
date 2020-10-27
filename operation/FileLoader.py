cc1c8a2a6fa78ba911ea2f762ebd6e5828408150
slawekrom <slawekrom@op.pl>
2020-10-27 17:16:04 +0100

    Obsługa komentarzy

--- operation/FileLoader.py
+++ operation/FileLoader.py
@@ -3,5 +3,5 @@ import pandas as pd
 class FileLoader:
 
     def loadFile(self, file_path: str, separator: str, col_index):
-        df = pd.read_csv(file_path, separator, index_col=None)
+        df = pd.read_csv(file_path, separator, index_col=None, comment='#')
         return df
